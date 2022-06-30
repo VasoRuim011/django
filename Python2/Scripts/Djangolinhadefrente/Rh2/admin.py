fec319b64b3a3fe4707828cba6db801238726cf9
VasoRuim011 <106943281+VasoRuim011@users.noreply.github.com>
2022-06-29 21:38:02 -0300

    atualizado

---
+++ Python2/Scripts/Djangolinhadefrente/Rh2/admin.py
@@ -0,0 +1,5 @@
+from .models import Funcionario
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Funcionario)
---
+++ Python2/Scripts/Djangolinhadefrente/Rh2/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Funcionario(models.Model):
+    CARGOS = [
+        ('ES', 'Estagiario'), 
+        ('AS', 'Assistente'),
+        ('JR', 'Junior'),
+        ('PL', 'Pleno'),
+        ('SR', 'Senior'),
+        ('GR', 'Gerente')
+
+
+
+    ]
+
+    matricula = models.IntegerField()
+    nome = models.CharField(max_length=50)
+    cargo = models.CharField(max_length=2, choices=CARGOS)
+    departamento = models.CharField(max_length=30)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    data_nascimento = models.DateField(null=True)
