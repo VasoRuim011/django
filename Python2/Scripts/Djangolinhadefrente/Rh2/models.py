9f8bc7ff398cdd2176d3841679d453662315d8c0
VasoRuim011 <106943281+VasoRuim011@users.noreply.github.com>
2022-07-04 22:25:40 -0300

    aula 04/07

--- Python2/Scripts/Djangolinhadefrente/Rh2/models.py
+++ Python2/Scripts/Djangolinhadefrente/Rh2/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Departamento(models.Model):
+    nome = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.nome
+
+
 class Funcionario(models.Model):
         
     CARGOS = [
@@ -15,20 +23,10 @@ class Funcionario(models.Model):
 
     matricula = models.IntegerField()
     nome = models.CharField(max_length=50)
-    cargo = models.CharField(max_length=2, choices=CARGOS)
-    departamento = models.CharField(max_length=30)
+    cargo = models.CharField(max_length=20, choices=CARGOS)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     salario = models.DecimalField(max_digits=10, decimal_places=2)
     data_nascimento = models.DateField(null=True)
 
     def __str__(self):
         return self.nome
-
-
-class Departamento(models.Model):
-    nome = models.CharField(max_length=20)
-
-
-    def __str__(self):
-        return self.nome
-
-
