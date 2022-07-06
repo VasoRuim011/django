ac3e9a78f780a0897542c5a7798d590d52c1a9b3
VasoRuim011 <106943281+VasoRuim011@users.noreply.github.com>
2022-07-05 21:02:01 -0300

    branch

--- Python2/Scripts/Djangolinhadefrente/Rh2/models.py
+++ Python2/Scripts/Djangolinhadefrente/Rh2/models.py
@@ -1,3 +1,5 @@
+from random import choices
+from time import time
 from django.db import models
 
 # Create your models here.
@@ -25,8 +27,18 @@ class Funcionario(models.Model):
     nome = models.CharField(max_length=50)
     cargo = models.CharField(max_length=20, choices=CARGOS)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_hora = models.DecimalField(max_digits=10, decimal_places=2)
     data_nascimento = models.DateField(null=True)
+    horas_trabalhadas = models.TimeField(choices=time)
 
     def __str__(self):
         return self.nome
+
+    def valor_hora:
+
+            valor_hora = float(input("Digite o valor ganho por hora de trabalho: "))
+            horas_trabalhadas = float(input("Digite o valor de horas trabalhadas neste mês: "))
+            pagamento = valor_hora * horas_trabalhadas
+            print("Você trabalhou por",horas_trabalhadas,"horas.")
+            print("O valor da hora trabalhada é de R$",valor_hora)
+            print("O pagamento que deve ser recebido é de: R$",pagamento)
