f4687adafb94b2d997012f5b05dcf07f79295044
VasoRuim011 <106943281+VasoRuim011@users.noreply.github.com>
2022-07-11 21:28:44 -0300

    root

--- Python2/Scripts/Djangolinhadefrente/Djangolinhadefrente/settings.py
+++ Python2/Scripts/Djangolinhadefrente/Djangolinhadefrente/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Djangolinhadefrente.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE' : 'django.dbe.backends.mysql',
+       'NAME' : 'BD_RH',
+       'USER' : 'root',
+       'HOST' : 'localhost',
+       'PORT' : '3306'
     }
 }
 
